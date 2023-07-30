c64c7ee7998b50e65a6b478da3d42382afd367ac
haoyu ma <mahaoyu965@gmail.com>
2023-07-31 00:20:05 +1000

    Conv1D
    build Conv1D model

---
+++ HaoYu Ma/Conv1D.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras import models
+from keras import layers
+from keras.callbacks import ReduceLROnPlateau
+import tensorflow
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+
+df = pd.read_csv("features_simple.csv")
+df = df.fillna(0)
+
+X = df.iloc[:][: -1]
+Y = df.iloc[:][-1]
+
+lb = LabelEncoder()
+Y = lb.fit_transform(Y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=42, test_size=0.2, shuffle=True)
+X_train.shape, X_test.shape, y_train.shape, y_test.shape
+
+# Standardize data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# We have to use 1-dimensional CNN which need specifical shape:
+X_train = np.expand_dims(X_train, axis=2)
+X_test = np.expand_dims(X_test, axis=2)
+
+earlystopping = EarlyStopping(monitor ="val_acc", mode = 'auto', patience = 5, restore_best_weights = True)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', patience=3, verbose=1,factor=0.5, min_lr=0.00001)
+
+
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+
+
+model = models.Sequential()
+model.add(layers.Conv1D(512, kernel_size=5, strides=1,
+                        padding="same", activation="relu",
+                        input_shape=(X_train.shape[1], 1)))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool1D(pool_size=5, strides=2, padding="same"))
+
+model.add(layers.Conv1D(512, kernel_size=5, strides=1,
+                        padding="same", activation="relu"))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool1D(pool_size=5, strides=2, padding="same"))
+
+model.add(layers.Conv1D(256, kernel_size=5, strides=1,
+                        padding="same", activation="relu"))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool1D(pool_size=5, strides=2, padding="same"))
+
+model.add(layers.Conv1D(256, kernel_size=3, strides=1, padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling1D(pool_size=5, strides = 2, padding = 'same'))
+
+model.add(layers.Conv1D(128, kernel_size=3, strides=1, padding='same', activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling1D(pool_size=3, strides = 2, padding = 'same'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(7, activation="softmax"))
+
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["acc", f1_m])
+
+model.summary()
+
+EPOCHS = 50
+batch_size = 64
+
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
+                    epochs=EPOCHS, batch_size=batch_size,
+                    callbacks=[earlystopping, learning_rate_reduction])
